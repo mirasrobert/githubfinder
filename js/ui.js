15a81e02105e34bc0c95ecf9db03fe349ad7a609
mirasrobert <mrmirasrobert@gmail.com>
2021-02-25 13:44:33 +0800

    Updates

---
+++ js/ui.js
@@ -0,0 +1,100 @@
+class UI 
+{
+    constructor() {
+        this.profile = document.getElementById('profile');
+    }
+
+    // Show User UI
+    showProfile(user) {
+        this.profile.innerHTML = `
+        <div class="card card-body mb-3">
+            <div class="row">
+                <div class="col-md-3">
+                    <img class="img-fluid mb-2" src="${user.avatar_url}">
+                    <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+                </div>
+                <div class="col-md-9">
+                    <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+                    <span class="badge badge-warning">Public Gists: ${user.public_gists}</span>
+                    <span class="badge badge-success">Followers: ${user.followers}</span>
+                    <span class="badge badge-info">Following: ${user.following}</span>
+                    <br><br>
+                    <ul class="list-group">
+                        <li class="list-group-item">Email: ${user.email}</li>
+                        <li class="list-group-item">Company: ${user.company}</li>
+                        <li class="list-group-item">Website/Blog: ${user.blog}</li>
+                        <li class="list-group-item">Location: ${user.location}</li>
+                        <li class="list-group-item">Member since: ${user.created_at}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <h3 class="page-heading mb-3">Latest Repos</h3>
+        <div id="repos"></div>
+        `;
+    }
+
+    // Show user repos
+    showRepos(repos) {
+        let output = '';
+
+        repos.forEach(repo => {
+            output += `
+                <div class="card card-body mb-2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                        </div>
+                        <div class="col-md-6">
+                            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                            <span class="badge badge-warning">Watchers ${repo.watchers_count}</span>
+                            <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+
+        // Output the repo
+        document.getElementById('repos').innerHTML = output;
+    }
+
+    // Clear user profile
+    clearProfile() {
+        this.profile.innerHTML = '';
+    }
+
+    // Show an alert message
+    showAlert(message, className) {
+        // Remove the remaining alert
+        this.clearAlert();
+        // Create a div
+        const div = document.createElement('div');
+        // Add a class
+        div.className = className;
+        // Add a text
+        div.appendChild(document.createTextNode(message));
+        // Get parent
+        const container = document.querySelector('.searchContainer');
+        // Get search box
+        const search = document.querySelector('.search');
+        // Insert alert
+        container.insertBefore(div, search);
+
+        // Remove the alert after 3secs 
+        setTimeout(() => {
+            this.clearAlert();
+        },3000);
+
+    }
+
+    clearAlert() {
+
+        const currentAlert = document.querySelector('.alert');
+
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+}
