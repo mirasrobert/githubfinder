8f66bc65d51e083819a51127d5ce9513c9f87c2a
mirasrobert <mrmirasrobert@gmail.com>
2021-03-01 09:56:42 +0800

    Updates

--- js/github.js
+++ js/github.js
@@ -1,24 +1,22 @@
-class Github 
-{
-    constructor() {
-        this.client_id = "9a328df8fc89f6e6e3d9";
-        this.client_secret = "95d5e4387b75f59be8615930f378bace8ff5cc9b";
-        this.repos_count = 10;
-        this.repos_sort = 'created: asc';
-    }
+class Github {
+  constructor() {
+    this.repos_count = 10;
+    this.repos_sort = "created: asc";
+  }
 
-    async getUser(user) {
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}`);
 
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-        
-        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
-        
-        const profile = await profileResponse.json();
-        const repos = await repoResponse.json();
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos`
+    );
 
-        return {
-            profile,
-            repos
-        };
-    }
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
 }
